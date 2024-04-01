adecaebd2fcd05b4ca4a22966fdca155b32e3c1c
Niklas <niklas_edelstam@hotmail.com>
2024-04-02 00:47:40 +0200

    first commit

---
+++ FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FriendStorage.UI.ViewModel
+{
+  public class MainViewModel : ViewModelBase
+  {
+        public MainViewModel()
+        {
+           NavigationViewModel = new NavigationViewModel();
+        }
+
+        public NavigationViewModel NavigationViewModel { get; private set; }
+
+        public void Load()
+        {
+            NavigationViewModel.Load();
+        }
+    }
+}
