19ba8a4b3cdd981d146301567ca41cb18ff858d8
Niklas <niklas_edelstam@hotmail.com>
2024-04-02 00:55:07 +0200

    test ShouldLoadFriends

--- FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
+++ FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using FriendStorage.UI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace FriendStorage.UITests.ViewModel
 {
     public class NavigationViewModelTests
     {
-            
+        [Fact]
+        public void ShouldLoadFriends()
+        {
+            var viewModel = new NavigationViewModel();
+
+            viewModel.Load();
+
+            //TODO: assert this
+            //Assert.Equal(2, viewModel.Friends.Count);
+        }
     }
 }
